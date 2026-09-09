3608a68f5e70a4c360b4100a28dd09b3e78fd87c
Riu <echo.soraku@gmail.com>
2026-09-09 09:58:35 +0700

    Update bot invite link with login scope and new permissions

--- src/utilities/Links.ts
+++ src/utilities/Links.ts
@@ -1,5 +1,5 @@
 export const RYNOTE_INVITE =
-  'https://discord.com/oauth2/authorize?client_id=1496804643530080376&permissions=4855449856833872&integration_type=0&scope=bot+applications.commands'
+  'https://discord.com/oauth2/authorize?client_id=1496804643530080376&permissions=687261216064&response_type=code&redirect_uri=https%3A%2F%2Frynote.apps.bot-hosting.cloud%2Fapi%2Fauth%2Fcallback%2Fdiscord&integration_type=0&scope=identify+email+guilds+bot+applications.commands'
 
 export const RYNOTE_SUPPORT = 'https://discord.gg/MsxdNeExdg'
 export const RYNOTE_GITHUB = 'https://github.com/Rynexid'
