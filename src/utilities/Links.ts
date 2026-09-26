f54747104077106c988dbdc045efd611fe53e56d
Riu <echo.soraku@gmail.com>
2026-09-26 14:08:40 +0700

    chore: add ManageChannels permission bit to bot invite link

--- src/utilities/Links.ts
+++ src/utilities/Links.ts
@@ -1,5 +1,5 @@
 export const RYNOTE_BOT_INVITE =
-  'https://discord.com/oauth2/authorize?client_id=1022891520019419239&permissions=687261216064&integration_type=0&scope=bot+applications.commands'
+  'https://discord.com/oauth2/authorize?client_id=1022891520019419239&permissions=687261216080&integration_type=0&scope=bot+applications.commands'
 
 export const RYNOTE_LOGIN =
   'https://discord.com/oauth2/authorize?client_id=1022891520019419239&response_type=code&redirect_uri=https%3A%2F%2Frynote.apps.bot-hosting.cloud%2Fapi%2Fauth%2Fcallback%2Fdiscord&scope=identify+email+guilds'
